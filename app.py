8da2a62b4470f1f6bd0d588d7500371e30cfbebf
Cristhian Fonseca <crisfon6>
2021-05-08 20:03:01 -0500

    works

--- app.py
+++ app.py
@@ -5,15 +5,11 @@ import pandas as pd
 import wx
 
 eel.init('web')
+errors = []
+
 
 @eel.expose
-def add(proxies,models,credentials):
-    print(proxies)
-    
-    return 3
-
-@eel.expose
-def pythonFunction(wildcard="*"):
+def read_csv(wildcard="*",id):
     app = wx.App(None)
     style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
     dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
@@ -21,7 +17,31 @@ def pythonFunction(wildcard="*"):
         path = dialog.GetPath()
     else:
         path = None
-    dialog.Destroy()
-    return path
+    resp = ''
+    path.split('/')
+    print('/')
+    try:
+        df = pd.read_csv(path)
 
-eel.start('index.html' ,size=(600, 600))
+        df.columns
+        resp = {
+            "msg": "Cargado Correctamente",
+            "type": "ok"}
+
+    except:
+        resp = {
+            "msg": "Archivo no aceptado por favor revise que el archivo cumpla con las condiciones",
+            "type": "error"}
+    dialog.Destroy()
+    errors.append(resp)
+    return resp
+
+
+@eel.expose
+def run(timeawait, amountproxies):
+    print(timeawait)
+    print(amountproxies)
+    return errors
+
+
+eel.start('index.html', size=(600, 600))
--- web/main.js
+++ web/main.js
@@ -1,19 +1,41 @@
-let btn = document.getElementById('run');
+// csv
+let proxies = document.getElementById('proxies');
+let credentials = document.getElementById('credentials');
+let models = document.getElementById('models');
+
+// send
+let run = document.getElementById('run');
+
+const btns = [proxies, credentials, models];
+const response = (resp, id) => {
+    console.log(resp);
+    let element = document.getElementById(id);
+    alert.innerHTML = resp['msg'];
+    if (resp['type'] === 'error') {
+        element.classList.add("text-danger");
+    } else {
+        element.classList.add("text-success");
+    }
+}
+
+btns.forEach(async(el) => {
+    el.addEventListener('click', async() => {
+        await eel.read_csv();
+    });
+});
 
 
+run.addEventListener('click', () => {
+    // variables+
+    let proxies = 0;
+    let amountproxie = document.getElementsByClassName('form-check-input');
+    if (amountproxie[0].checked) {
+        proxies = 1;
+    } else {
+        proxies = 4;
+    }
+    let timeawait = document.getElementById('timeawait').value;
 
-btn.addEventListener('click', () => {
-    eel.pythonFunction()(r => console.log(r));
-    // const proxies = document.getElementById('proxies').files;
-    // const credentials = document.getElementById('credentials').files;
-    // const models = document.getElementById('models').files;
-
-
-
-    // console.log(proxies);
-    // eel.add(proxies, models, credentials)(draw)
+    eel.run(timeawait, proxies)(r => console.log(r));
 
 });
-const draw = (data) => {
-    document.getElementById('screen').innerHTML = data
-}
