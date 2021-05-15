50fa810461aae4b4ff37c81bd92bef47b42034c6
Cristhian Fonseca <cdelgado376@unab.edu.co>
2021-05-15 14:46:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt-get update --y
 sudo chmod +x ./drivers/chromedriver
 
-sudo apt install python3-tk --y
+sudo apt install python3-tk 
 pip3 install tk
 pip3 install selenium
 pip3 install pandas
